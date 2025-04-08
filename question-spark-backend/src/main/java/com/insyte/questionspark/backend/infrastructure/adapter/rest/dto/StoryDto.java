f3d63e4db864847e32030d7912facd8619647c31
Pranay Narotam <pnarotam@gmail.com>
2025-04-08 14:08:28 +1200

    deleted storyDto

--- question-spark-backend/src/main/java/com/insyte/questionspark/backend/infrastructure/adapter/rest/dto/StoryDto.java
+++
@@ -1,8 +0,0 @@
-package com.insyte.questionspark.backend.infrastructure.adapter.rest.dto;
-
-import java.time.LocalDateTime;
-import java.util.UUID;
-
-public record StoryDTO(UUID id, String title, String description, LocalDateTime createdAt) {
-    
-}
